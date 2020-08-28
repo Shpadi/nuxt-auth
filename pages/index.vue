c1d9bc70c3d43efd48e2192aeb6d64e59b29b2dd
VladislavShpadii <shpad1997@gmail.com>
2020-08-28 16:38:54 +0300

    Project Setup

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+      <div class="text-center">
+      </div>
+      <v-card>
+      
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  }
+}
+</script>
---
+++ pages/login.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-row class="pa-5">
+        <v-card class="pa-5"  width="100%">
+            <v-form>
+                <v-text-field />
+                <v-text-field />
+                <v-btn color="primary">Login</v-btn>
+            </v-form>
+        </v-card>
+    </v-row>
+</template>
+<script>
+    export default {
+        layout: 'login'
+    }
+</script>
